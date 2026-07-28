58c67b8771639bab7940cdfc6a7550cf4b2cc46c
JosediS07 <jdsanchezm05@gmail.com>
2026-07-28 11:43:33 +0200

    feat(empresa): agregar EmpresaRepository

--- src/main/java/com/huntlog/empresa/EmpresaRepository.java
+++ src/main/java/com/huntlog/empresa/EmpresaRepository.java
@@ -1,4 +1,10 @@
 package com.huntlog.empresa;
 
-public interface EmpresaRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    Page<Empresa> findByUsuarioId(Long usuarioId, Pageable pageable);
 }
