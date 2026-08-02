32296874d5344447a4a2d05b32e0dbea8fa3e3a7
JosediS07 <jdsanchezm05@gmail.com>
2026-08-02 18:41:15 +0200

    feat(busqueda): crear BusquedaPort

--- src/main/java/com/huntlog/busqueda/port/BusquedaPort.java
+++ src/main/java/com/huntlog/busqueda/port/BusquedaPort.java
@@ -1,4 +1,10 @@
 package com.huntlog.busqueda.port;
 
+import com.huntlog.busqueda.dto.OfertaExternaResponse;
+
+import java.util.List;
+
 public interface BusquedaPort {
+
+    List<OfertaExternaResponse> buscar(String query, String pais);
 }
